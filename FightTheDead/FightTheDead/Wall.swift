02e072508ab7ae5e482988b0f38a1763fd7ed020
zooTheBear <36935293+zooTheBear@users.noreply.github.com>
2018-04-25 21:01:25 -0400

    Update Wall.swift

--- FightTheDead/FightTheDead/Wall.swift
+++ FightTheDead/FightTheDead/Wall.swift
@@ -10,8 +10,9 @@ import Foundation
 import SpriteKit
 
 class Wall : GameObject {
-    
+    //walls health
     var health: CGFloat = 400
+    //sets to true if the wall is broken
     var dead = false
     init() {
         super.init(imageName: "wall1")
@@ -22,19 +23,19 @@ class Wall : GameObject {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+    //update fucntion
     override func update(_ currentTime: TimeInterval) {
         super.update(currentTime)
         guard let scene = parent else {
             return
         }
-        
+        //if health is below 0 sets the wall to dead
         if(self.health <= 0)
         {
             self.dead = true
         }
     }
-    
+    //takes damge based on the argument
     func takeDamge(damgeAmount: CGFloat){
         self.health -= damgeAmount
     }
